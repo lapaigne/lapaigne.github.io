ddde3ee79c3034add09ffc387746e9277c73f25f
Oleg Lialin <lyalin.pa1nt3r@gmail.com>
2022-12-14 18:24:31 +0400

    alpha

---
+++ js/misc/main.js
@@ -0,0 +1,10 @@
+﻿window.onload = function () {
+    document.querySelectorAll('.topbar-btn').forEach(e => e.addEventListener('click', topbarHandler));
+    calculate();
+    poly();
+}
+
+function topbarHandler(event) {
+    document.querySelectorAll('.content-item').forEach(e => e.classList.add('hidden'));
+    document.getElementById(event.target.dataset.content).classList.remove('hidden');
+}
---
+++ js/polynomial/PolynomialMenu.js
@@ -0,0 +1,14 @@
+﻿function poly() {
+    function operandHandler(e) {
+        const calc = new PolynomialCalculator();
+        const num1 = document.getElementById('poly-num1');
+        const num2 = document.getElementById('poly-num2');
+        const a = calc.getPolynomial(num1.value);
+        const b = calc.getPolynomial(num2.value);
+        const operand = e.target.dataset.operand;
+        const c = calc[operand](a, b);
+        document.getElementById('poly-result').innerHTML = (c) ? (c.toString() ? c.toString() : '0') : 'Ошибка';
+    }
+    document.querySelectorAll('.poly-op').
+        forEach(e => e.addEventListener('click', operandHandler));
+}
